8b6cb3ae39f41de942c42d26750d33984367e5b5
Claudio Alvarez <claudio.alvarez@inzpiral.com>
2014-09-15 15:33:47 -0300

    nueva gema para paginar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.xml
   def index
-    @events = Event.all
+    @events = Event.paginate(:page => params[:page], :per_page => 20)
 
     respond_to do |format|
       format.html # index.html.erb
